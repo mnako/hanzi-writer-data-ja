f7da0f660166a5c7562d296726225b89626a9f56
Michal Nakoneczny <michal.nakoneczny@lionbridge.com>
2021-04-12 23:10:30 +0900

    Add Github Action

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,8 +1,5 @@
-apt-get install -y jq
-git clone --depth 1 https://github.com/parsimonhi/animCJK
 while IFS= read -r line; do
     printf '%s' "$line" | jq -c -r ".character"
     printf '%s' "$line" | jq -c "{strokes: .strokes, medians: .medians}" > data/`printf '%s' "$line" | jq -r ".character"`.json
 done < animCJK/graphicsJA.txt
-rm data/.json
-rm -rf animCJK
+
