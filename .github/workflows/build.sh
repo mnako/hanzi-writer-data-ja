56bed9b83b3bd4d72351f4fdca02140d55878db4
Michal Nakoneczny <michal.nakoneczny@lionbridge.com>
2021-04-12 23:27:17 +0900

    Add Github Action

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,6 +1,3 @@
-pwd
-ls -la
-ls -la animCJK
 while IFS= read -r line; do
     printf '%s' "$line" | jq -c -r ".character"
     printf '%s' "$line" | jq -c "{strokes: .strokes, medians: .medians}" > data/`printf '%s' "$line" | jq -r ".character"`.json
